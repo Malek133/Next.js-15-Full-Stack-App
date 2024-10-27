e53021a2d93f451f8bcd8d0a0af857bfe40c5fe0
unknown <amazighmalek.com>
2024-10-27 17:16:53 +0100

    install project and auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">
+        Mon application Next.js 15 with JSMASTERY</h1>
+      
+    </div>
+  )
+}
